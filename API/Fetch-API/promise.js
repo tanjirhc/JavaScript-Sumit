d889386d2248e9f35d35236c0ea99b4f685b1112
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-05 20:48:48 +0600

    Asynchronous behaviour checking

--- API/Fetch-API/promise.js
+++ API/Fetch-API/promise.js
@@ -3,6 +3,10 @@ function getData() {
   fetch("http://127.0.0.1:5500/API/Fetch-API/data.txt")
     .then(res => res.text())
     .then(data => {
+      console.log("hello 2");
       display.innerText = data;
     })
+
+
+    console.log("hello");
 }
