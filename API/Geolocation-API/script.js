86310758dba3755410ea3ab0fefb05249a63c276
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-05 21:21:51 +0600

    different errors show

--- API/Geolocation-API/script.js
+++ API/Geolocation-API/script.js
@@ -13,5 +13,18 @@ function showPosition(position){
 }
 
 function showError(error) {
-  console.log(error);
-}
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      display.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      display.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      display.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      display.innerHTML = "An unknown error occurred."
+      break;
+  }
+} 
