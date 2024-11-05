829f6746837dd1aec66bbcd59c183988ff382d5b
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-05 20:45:02 +0600

    data text convert

--- API/Fetch-API/promise.js
+++ API/Fetch-API/promise.js
@@ -1,4 +1,5 @@
 const display = document.getElementById("display");
 function getData() {
-  fetch("http://127.0.0.1:5500/API/Fetch-API/data.txt");
+  fetch("http://127.0.0.1:5500/API/Fetch-API/data.txt")
+    .then(res => res.text());
 }
