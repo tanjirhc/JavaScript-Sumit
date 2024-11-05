d33f5e885547a6d042d8fc8121dd486efff0da54
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-05 20:47:00 +0600

    display data after promise

--- API/Fetch-API/promise.js
+++ API/Fetch-API/promise.js
@@ -1,5 +1,8 @@
 const display = document.getElementById("display");
 function getData() {
   fetch("http://127.0.0.1:5500/API/Fetch-API/data.txt")
-    .then(res => res.text());
+    .then(res => res.text())
+    .then(data => {
+      display.innerText = data;
+    })
 }
