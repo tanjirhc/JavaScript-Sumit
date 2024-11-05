d05421aef777c70f23da175b92ffba4eab928d46
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-05 14:28:20 +0600

    custom validity

--- API/Form-API/script.js
+++ API/Form-API/script.js
@@ -1,6 +1,14 @@
 function validation () {
   const inputObj = document.getElementById('id1');
 
+  if(inputObj.validity.rangeOverflow) {
+    inputObj.setCustomValidity("You have made a range overflow error!");
+  } else if (inputObj.validity.rangeUnderflow) {
+    inputObj.setCustomValidity("You have made a range underflow error!");
+  } else if (inputObj.validity.valueMissing) {
+    inputObj.setCustomValidity("Value Missing");
+  }
+
   if (!inputObj.checkValidity()) {
     document.getElementById("demo").innerHTML = inputObj.validationMessage;
   }
