94c8caa9a05cec2455b7e5cb1c571226055d03ef
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-06 19:20:52 +0600

    return new Date()

--- JSON/lession-3/script.js
+++ JSON/lession-3/script.js
@@ -3,8 +3,10 @@ const jsonString = '{"name":"John", "birth": "1986-12-14", "city":"New York"}';
 console.log(jsonString);
 // conver to valid js array
 const myObj = JSON.parse(jsonString, function(key, value) {
-  console.log(`${key} - ${value}`);
-  console.log("--------");
+  if(key === 'birth') {
+    return new Date(value);
+  } 
+  return value;
 });
 
 console.log(new Date(myObj.birth));
