626927e94dd685cb693fc10263d862d76c6a8564
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-06 17:56:38 +0600

    JSON Data format support

--- JSON/lession-1/script.js
+++ JSON/lession-1/script.js
@@ -2,6 +2,7 @@ const person = {
   name: "Tanjir",
   age: 38,
   skills: ["JavaScript", "Java", "C sharp"],
+  birthday: new Date('13-08-1985'),
 };
 
-console.log(person);
+console.log(JSON.stringify(person));
