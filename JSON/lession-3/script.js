866edd672b97394eb63b55d13ee20bdf1ffacde7
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-06 19:17:49 +0600

    key, value function

--- JSON/lession-3/script.js
+++ JSON/lession-3/script.js
@@ -2,6 +2,9 @@ const jsonString = '{"name":"John", "birth": "1986-12-14", "city":"New York"}';
 
 console.log(jsonString);
 // conver to valid js array
-const myObj = JSON.parse(jsonString);
+const myObj = JSON.parse(jsonString, function(key, value) {
+  console.log(`${key} - ${value}`);
+  console.log("--------");
+});
 
 console.log(new Date(myObj.birth));
