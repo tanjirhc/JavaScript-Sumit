658cda4a9224ea620204a824602b024d53333304
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-07 00:20:18 +0600

    parsing not needed in fetch API calling

--- JSON/lession-5/script.js
+++ JSON/lession-5/script.js
@@ -1,8 +1,8 @@
 async function fetchData() {
   const response = await fetch("demo.txt");
 
-  const data = await response.text();
-  console.log(data);
+  const data = await response.json();
+  console.log(data.name); // does not need to parse data
 }
 
 fetchData();
