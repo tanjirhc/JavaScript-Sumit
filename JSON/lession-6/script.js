9cc10cc19685c2a89ac65cecfcf834889b6e3443
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-07 22:36:06 +0600

    now ok

--- JSON/lession-6/script.js
+++ JSON/lession-6/script.js
@@ -1,12 +1,12 @@
 async function change_myselect(sel) {
-  // const dbParam = JSON.stringify({table:sel,limit:20});
+  const dbParam = JSON.stringify({table:sel,limit:20});
 
   const response = await fetch("https://www.w3schools.com/js/json_demo_html_table.php", {
     method: "POST",
     headers: {
       "Content-type": "application/x-www-form-urlencoded"
     },
-    body: JSON.stringify();
+    body: "x=" + JSON.stringify(dbParam),
   });
 
 
